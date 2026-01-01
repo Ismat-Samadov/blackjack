a37ada2b2d256544752dc5f697408039895780cf
ismat-samadov <ismetsemedov@gmail.com>
2026-01-01 20:33:39 +0400

    init

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,9 @@ import "./globals.css";
 export const metadata: Metadata = {
   title: "Blackjack Game",
   description: "Play Blackjack - Hit, Stand, and Track Your Chips!",
+  icons: {
+    icon: "/icon.svg",
+  },
 };
 
 export default function RootLayout({
